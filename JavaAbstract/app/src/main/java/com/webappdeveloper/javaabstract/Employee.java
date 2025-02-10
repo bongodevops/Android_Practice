2e6402b683719fc5c561193151a09b39da4e5dae
bongodevops <bongodevops@gmail.com>
2025-02-10 21:55:57 +0600

    upload resource

---
+++ JavaAbstract/app/src/main/java/com/webappdeveloper/javaabstract/Employee.java
@@ -0,0 +1,33 @@
+package com.webappdeveloper.javaabstract;
+
+public abstract class Employee {
+   private final String Name;
+   private final String Position;
+   private final Float Salary;
+
+   // if abstract method has no body
+   public abstract float calculateTax();
+   public abstract float finalSalary();
+
+    //constructor create
+    public Employee(String emName,String emPosition,float emSalary){
+
+        this.Name = emName;
+        this.Position =emPosition;
+        this.Salary= emSalary;
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public Float getSalary() {
+        return Salary;
+    }
+}
+
